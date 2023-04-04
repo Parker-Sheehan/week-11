0a1d154f1b1f36eb447f1537f6121354313fc43b
Parker Sheehan <Parkersheehan007@gmail.com>
2023-04-03 21:21:01 -0500

    events project

--- 12-adv-starting-project/frontend/src/App.js
+++ 12-adv-starting-project/frontend/src/App.js
@@ -20,8 +20,29 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import React, {Fragment} from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import EventsPage from './components/EventsPage'
+import EventDetailPage from './components/EventDetailPage'
+import NewEventPage from './components/NewEventPage'
+import EditEventPage from './components/EditEventPage'
+import MainNavigation from "./components/MainNavigation";
+
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <MainNavigation/>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/events" element={<EventsPage />}></Route>
+        <Route path="/events/:id" element={<EventDetailPage />}></Route>
+        <Route path="/events/new" element={<NewEventPage />}></Route>
+        <Route path="/events/:id/edit" element={<EditEventPage />}></Route>
+
+      </Routes>
+    </Fragment>
+  );
 }
 
 export default App;
